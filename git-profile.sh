f9aa7ca856bb488dfb6365c7dba72f22a7aa08dc
Francesco Bianco <info.francescobianco@gmail.com>
2020-06-27 13:14:48 +0200

    Forkfile start...

--- git-profile.sh
+++ git-profile.sh
@@ -1,18 +1,73 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+##
+# git-profile
+#
+# Manage your profile with GIT.
+#
+# Copyright (c) 2020 Francesco Bianco <bianco@javanile.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+##
+
 set -e
 
 home=${HOME}/.gitprofile
 
+##
+#
+##
+usage () {
+    echo "Usage: git profile [--help] <command> [<args>]"
+    echo ""
+    echo "Executes FILE as a test case also collect each LCOV info and generate HTML report"
+    echo ""
+    echo "List of available options"
+    echo "  -e, --extension EXT     Coverage of every *.EXT file (default: sh)"
+    echo "  -i, --include PATH      Include files matching PATH"
+    echo "  -x, --exclude PATH      Exclude files matching PATH"
+    echo "  -o, --output OUTDIR     Write HTML output to OUTDIR"
+    echo "  -h, --help              Display this help and exit"
+    echo "  -v, --version           Display current version"
+    echo ""
+    echo "Documentation can be found at https://github.com/javanile/lcov.sh"
+}
+
+##
+#
+##
 error () {
     echo "profile: $2"
     exit $1
 }
 
+##
+#
+##
 git_profile_init () {
     [[ -d ${home} ]] || mkdir -p ${home}
     cd ${home}
 }
 
+##
+#
+##
 git_profile_clone () {
     git_profile_init
     [[ -z "$1" ]] && error 1 "missing profile name"
@@ -22,6 +77,9 @@ git_profile_clone () {
     [[ -d current ]] || ln -s $2 current
 }
 
+##
+#
+##
 git_profile_use () {
     git_profile_init
     [[ -z "$1" ]] && error 1 "missing profile name"
@@ -30,6 +88,9 @@ git_profile_use () {
     ln -s $1 current
 }
 
+##
+#
+##
 git_profile_edit () {
     git_profile_init
     #[[ -z "$1" ]] && error 1 "missing profile name"
@@ -42,15 +103,22 @@ git_profile_edit () {
     git push
 }
 
+##
+#
+##
 git_profile_ls () {
     git_profile_init
     ls -l
 }
 
+##
+#
+##
 case $1 in
     use) git_profile_use $2 ;;
-    clone) git_profile_clone $2 $3 ;;
+    add|clone) git_profile_clone $2 $3 ;;
     edit) git_profile_edit $2 ;;
     ls) git_profile_ls ;;
+    --help) usage ;;
     *) error 1 "'$1' is not a profile command. See 'git profile --help'."
 esac
